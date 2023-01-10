ac256069d995d3a9b6761946d554dad89caa85df
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-01-10 20:40:03 +0900

    keystore added : change protocol http to https

--- src/main/java/com/cafe/waterdeer/WaterDeerApplication.java
+++ src/main/java/com/cafe/waterdeer/WaterDeerApplication.java
@@ -1,13 +1,15 @@
 package com.cafe.waterdeer;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WaterDeerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WaterDeerApplication.class, args);
 	}
-
 }
